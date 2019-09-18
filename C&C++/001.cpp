ecd5f8cab7e07c778968a54169e3c5321d52ec16
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-18 09:58:22 +0800

    Update 001.cpp

--- C&C++/001.cpp
+++ C&C++/001.cpp
@@ -7,7 +7,7 @@ int* twoSum(int* nums, int numsSize, int target) {
     int *a = (int*)malloc(2*sizeof(int));
     for(int i = 0;i<numsSize;i++)
     {
-        for(int j = i+1;(j<numsSize && j != i);j++)
+        for(int j = i+1;j<numsSize;j++)
         {
             if(nums[i] + nums[j] == target)
             {
