d47e32a1af35b7c06c3a3b6a5ede940569db99a2
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-19 15:02:30 +0800

    Update 011.py

--- Python/011.py
+++ Python/011.py
@@ -6,9 +6,9 @@ class Solution011(object):
         while i < j:
             maxval = max(maxval, min(height[i], height[j])*(j-i)) #找出最左边和最右边组成容器的容积
             if height[i] < height[j]: #短边不需要再考虑 因为此时只要选定短边 其他任选边的容积都不会大于maxval
-                i += 1 #短边不用考虑 考虑下一块木板
+                i += 1 #短边不用考虑 考虑下一块木板 往中间移动
             else:
-                j -= 1 #短边不用考虑 考虑下一块木板
+                j -= 1 #短边不用考虑 考虑下一块木板 往中间移动
         return maxval
 test011=Solution011()
 nums011=[1,8,6,2,5,4,8,3,7]
