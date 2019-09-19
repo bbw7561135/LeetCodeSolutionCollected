e480348c9c7ac848cdcd4879a6499abd9b6dc770
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-19 13:25:53 +0800

    Update 007.py

--- Python/007.py
+++ Python/007.py
@@ -1,7 +1,7 @@
 class Solution007(object):
     def reverse(self, x) :
         if x>=0:
-            x = int(str(x)[::-1])
+            x = int(str(x)[::-1]) #先转换为str再翻转过来 再转为整数即可
         else:
             x = -int(str(-x)[::-1])
         if x < 2147483647 and x >= -2147483648: #in the range of [-2^31,2^31-1]
