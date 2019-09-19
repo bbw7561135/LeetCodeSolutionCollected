b9954eb6d7841b31605eae8292465b35b9d4fe02
bbw7561135 <32274080+bbw7561135@users.noreply.github.com>
2019-09-19 14:59:15 +0800

    Update 011.py

--- Python/011.py
+++ Python/011.py
@@ -1,14 +1,14 @@
 class Solution011(object):
     def maxArea(self, height):
-        i = 0
-        j = len(height) - 1
+        i = 0 #最左边
+        j = len(height) - 1 #最右边
         maxval = 0
         while i < j:
-            maxval = max(maxval, min(height[i], height[j])*(j-i))
-            if height[i] < height[j]:
-                i += 1
+            maxval = max(maxval, min(height[i], height[j])*(j-i)) #找出最左边和最右边组成容器的容积
+            if height[i] < height[j]: #短边不需要再考虑 因为此时只要选定短边 其他任选边的容积都不会大于maxval
+                i += 1 #短边不用考虑 考虑下一块木板
             else:
-                j -= 1
+                j -= 1 #短边不用考虑 考虑下一块木板
         return maxval
 test011=Solution011()
 nums011=[1,8,6,2,5,4,8,3,7]
